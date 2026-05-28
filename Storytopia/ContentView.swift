08d4d61050206b0eafb283d3664ab28c810f7d32
Mike Kogan <mfkogan@gmail.com>
2026-05-28 15:58:50 -0400

    Initial Commit

---
+++ Storytopia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Storytopia
+//
+//  Created by Mike Kogan on 5/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Storytopia/StorytopiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorytopiaApp.swift
+//  Storytopia
+//
+//  Created by Mike Kogan on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct StorytopiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
